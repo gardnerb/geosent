614eaafe829a7e34ac55265ad884d1effa587cdd
gardnerb <gardnerb@umich.edu>
2014-04-03 00:45:04 -0400

    modified tweet collection to grab geo-tagged or user profile location tweets

--- pulltweets.py
+++ pulltweets.py
@@ -1,12 +1,27 @@
+"""
+    Reads authentication credentials from 'credentials.txt', which is formated as:
+
+        API key
+        API sercret
+        Access token
+        Access token secret
+
+    Pull tweets get tweets from the twitter Stream API,
+    using track as the keyword search
+"""
+
 from tweetlistener import TweetListener
 import time, tweepy, sys
 
+
+cred = open('credentials.txt')
+
 ## authentication
 username   = 'bgardnuh'
-api_key    = 'x'
-api_secret = 'x'
-key        = 'x'
-secret     = 'x'
+api_key    = cred.readline().strip()
+api_secret = cred.readline().strip()
+key        = cred.readline().strip()
+secret     = cred.readline().strip()
 auth       = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(key, secret)
 api        = tweepy.API(auth)
@@ -22,7 +37,7 @@ def main():
     try: 
         stream.filter(track = track)
     except:
-        print "error!"
+        print "error!", sys.exc_info()[0]
         stream.disconnect()
 
 if __name__ == '__main__':
--- tweetlistener.py
+++ tweetlistener.py
@@ -6,29 +6,32 @@ import json, time, sys
 class TweetListener(StreamListener):
 
 
-   cdef __init__(self, api = None, fprefix = 'streamer'):
+    def __init__(self, api = None, fprefix = 'streamer'):
         self.api = api or API()
         self.counter = 0
         self.fprefix = fprefix
         self.output  = open(fprefix + '.' 
-                        + time.strftime('%Y%m%d-%H%M%S') + '.json', 'w')
+                            + time.strftime('%Y%m%d-%H%M%S') + '.json', 'w')
         self.delout  = open('delete.txt', 'a')
 
 
     def on_data(self, data):
-        if 'in_reply_to_status' in data:
+        
+        jsondata = json.loads(data)
+        if jsondata["coordinates"] != None:
+            print "here!"
             self.on_status(data)
-        elif 'delete' in data:
-            delete = json.loads(data)['delete']['status']
-            if self.on_delete(delete['id'], delete['user_id']) is False:
-                return False
-        elif 'limit' in data:
-            if self.on_limit(json.loads(data)['limit']['track']) is False:
-                return False
-        elif 'warning' in data:
-            warning = json.loads(data)['warnings']
-            print warning['message']
-            return false
+            #not sure what these parts do
+            # if 'limit' in data:
+            #     if self.on_limit(json.loads(data)['limit']['track']) is False:
+            #         return False
+            # elif 'warning' in data:
+            #     warning = json.loads(data)['warnings']
+            #     print warning['message']
+            #     return False
+        elif jsondata["user"]["location"] != None:
+            print "user loc"
+            self.on_status(data)
 
 
     def on_status(self, status):
@@ -39,27 +42,24 @@ class TweetListener(StreamListener):
         if self.counter >= 20000:
             self.output.close()
             self.output = open('../streaming_data/' + self.fprefix + '.' 
-                               + time.strftime('%Y%m%d-%H%M%S') + '.json', 'w')
+                            + time.strftime('%Y%m%d-%H%M%S') + '.json', 'w')
             self.counter = 0
 
-        return
+        return True
 
-
-    def on_delete(self, status_id, user_id):
-        self.delout.write( str(status_id) + "\n")
-
-
-        return
 
     def on_limit(self, track):
         sys.stderr.write(track + "\n")
-        return
+        return True
+
 
     def on_error(self, status_code):
         sys.stderr.write('Error: ' + str(status_code) + "\n")
         return False
 
+
     def on_timeout(self):
         sys.stderr.write("Timeout, sleeping for 60 seconds...\n")
         time.sleep(60)
-        return 
+        return True
+
